41c6fbf93a685789a63b7e3a93bebc894640208c
rafalsilva05 <rafalsilva5002@gmail.com>
2024-09-30 21:42:03 -0300

    Adicionado interface para usuário e csv para usar como banco de dados

--- app.py
+++ app.py
@@ -5,6 +5,7 @@
 # First I will ask the user his name and a password
 
 import streamlit as st
+import pandas as pd
 
 # Application tittle
 st.title("Password_manager")
@@ -17,3 +18,27 @@ class User:
 
     def __str__(self) -> str:
         return f"{self.name}"
+    
+    def __dict__(self):
+        return {'name': self.name, 'password': list(self.pswd)}
+    
+
+    
+def add_user(username, password):
+    # new_user = User(username, password)
+    df = pd.DataFrame({'name': [username], 'password': [password]})
+    print(df)
+    df.to_csv("tabela.csv", index=False)
+    
+    
+
+# User inputs
+username = st.text_input("Enter your Username:")
+password = st.text_input("Enter your password:", type="password")
+
+if st.button("Create User:"):
+    if username and password:
+        add_user(username, password)
+        st.success(f"User {username} created")
+    else:
+        st.error("Please try again")
